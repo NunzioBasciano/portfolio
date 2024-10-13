9d9a1b67f4f193766cf4398876172b9db41fda54
NunzioBasciano <nbasho1988@gmail.com>
2024-10-13 23:37:50 +0200

    fix: Carousel layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Carousel from "./components/Carousel";
 export default function Home() {
   return (
     <div className="py-3 md:flex gap-3 justify-between">
-      <div className="py-4 px-6 flex flex-col gap-3 w-full sm:h-full shadow-[0_1px_2px_0_rgba(60,64,67,0.3),_0_2px_6px_2px_rgba(60,64,67,0.15)] rounded-2xl">
+      <div className="py-4 px-6 flex flex-col gap-3 w-full min-h-[full] shadow-[0_1px_2px_0_rgba(60,64,67,0.3),_0_2px_6px_2px_rgba(60,64,67,0.15)] rounded-2xl">
         <h1 className="text-white text-2xl my-3 font-semibold">
-          Nunzio basciano
+          Nunzio Basciano
         </h1>
         <h2 className="text-3xl mb-2 text-[var(--orange)]">
           Sviluppatore Frontend
@@ -33,7 +33,6 @@ export default function Home() {
           <strong className="text-[var(--orange)]">MongoDB</strong> e
           implementazione di CRUD, filtri dinamici e paginazione.
         </p>
-
         <p className="tracking-wide">
           Mi impegno costantemente a migliorare, unendo{" "}
           <strong className="text-[var(--orange)]">problem solving</strong> e

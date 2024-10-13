3a189e67657484e9cdee4ccf7f08187c1ca79d6b
NunzioBasciano <nbasho1988@gmail.com>
2024-10-13 21:02:24 +0200

    feat: socialLink and relatives links

---
+++ app/common/socialLink.ts
@@ -0,0 +1,7 @@
+export const socialLink = [
+    {label: 'Email', href: 'https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRwQnlShzBWZCSGCLXJbHGHRVlpJKRlWdXfgvJvxVsPNhZVplDDtkVgFNZJZgHvLkkgBPXfq', link: 'nunzio.basciano1988@gmail.com', imageSrc: "/gmail.png"},
+    {label: 'WhatsApp', href: 'https://wa.me/393339030272', link: '+39 333 90 30 272',  imageSrc: "/whatsapp.png"},
+    {label: 'LinkedIn', href: 'https://www.linkedin.com/in/nunzio-basciano/', link: '/nunzio-basciano/',  imageSrc: "/linkedin.png"},
+    {label: 'GitHub', href: 'https://github.com/NunzioBasciano', link: '/NunzioBasciano',  imageSrc: "/github.png"},
+
+  ];

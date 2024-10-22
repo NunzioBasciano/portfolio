69eac90eddc87f3be4afa6ec1957ec1c7016c8c6
NunzioBasciano <nbasho1988@gmail.com>
2024-10-22 15:17:17 +0200

    feat: dynamic carousel

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -1,29 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const cardList = [
-  { title: "Sicily Pulse", href: "/SicilyPulse.jpg" },
-  { title: "title 2", href: "https://via.placeholder.com/300" },
-  { title: "title 3", href: "https://via.placeholder.com/300" },
-  { title: "title 4", href: "https://via.placeholder.com/300" },
-  { title: "title 5", href: "https://via.placeholder.com/300" },
-  { title: "title 6", href: "https://via.placeholder.com/300" },
+  { id: "1", title: "Inventory Management", href: "/SicilyPulse.jpg" },
+  { id: "2", title: "Product Management", href: "/inventory-management.png" },
 ];
 
 function Carousel() {
-
   return (
-    <div className="max-md:mt-3 flex overflow-x-auto scroll-smooth snap-x snap-mandatory w-full min-h-[full] custom-scrollbar">
+    <div className="max-md:mt-3 flex overflow-x-auto scroll-smooth snap-x snap-mandatory w-full min-h-[full] custom-scrollbar rounded-xl">
       {cardList.map((item, index) => (
         <div
           key={index}
-          className="flex justify-center items-center w-full min-h-[full] flex-shrink-0 snap-start bg-gray-300 m-2"
+          className="relative bg-[var(--darBlue)] flex flex-col justify-center items-center w-full min-h-[full] flex-shrink-0 snap-start m-2"
         >
-          <h3>{item.title}</h3>
-          <img
-            src={item.href}
-            alt={item.title}
-            className="w-full h-full object-cover"
-          />
+          <div className="relative w-full  mx-auto h-64 md:h-full md:col-span-2 md:row-span-2 rounded-xl overflow-hidden">
+            <Link href={`/my-work/${item.id}`}>
+              <Image
+                src={item.href}
+                alt={item.title}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-xl bg-[var(--darkBlue)]"
+              />
+            </Link>
+          </div>
+          <h3 className="absolute top-[90%] text-[var(--darkBlue)] font-bold text-xl mt-2">
+            {item.title}
+          </h3>
         </div>
       ))}
     </div>

276219fb7affad9fbb676379bd1fc8097023bb40
NunzioBasciano <nunzio.basciano1988@gmail.com>
2024-11-07 19:25:56 +0100

    fix: page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { carouselListHome } from "./common/carouselListHome";
-import { homeArticle } from "./common/homeArticle";
-import ArticleGenerator from "./components/ArticleGenerator";
-import Button from "./components/Button";
-import Carousel from "./components/Carousel";
-import MainLayout from "./components/MainLayout";
+import { carouselListHome } from './common/carouselListHome';
+import { homeArticle } from './common/homeArticle';
+import ArticleGenerator from './components/ArticleGenerator';
+import Button from './components/Button';
+import Carousel from './components/Carousel';
+import MainLayout from './components/MainLayout';
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
         paragraphs={homeArticle.paragraphs}
       >
         <Button downloadCV={true} label="Scarica CV" />
-        <Button link="/my-project" label="Progetti" />
+        <Button link="/my-work" label="Progetti" />
         <Button link="/contact" label="Contatti" />
       </ArticleGenerator>
       <Carousel projects={carouselListHome} />
